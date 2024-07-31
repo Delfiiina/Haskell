4b069053b4a25450771af8b8f616a91ad661452e
Delfiboya <delfiboya@gmail.com>
2024-07-31 12:16:51 -0300

    Agrego los ejercicios

--- estudioAparte/ejerciciosChatGPT.hs
+++ estudioAparte/ejerciciosChatGPT.hs
@@ -1,1 +1,22 @@
-
+{--
+Ejercicio 1: Manipulación de listas
+Crea una función removeDuplicates que tome una lista y devuelva una nueva lista sin elementos duplicados. Solo puedes usar las funciones básicas de listas y concatenación
+removeDuplicates :: Eq a => [a] -> [a]
+Ejercicio 2: Análisis de listas
+Crea una función maxInList que encuentre el valor máximo en una lista de números utilizando solo las funciones básicas de listas.
+maxInList :: Ord a => [a] -> a
+Ejercicio 3: Números primos
+Escribe una función isPrime que determine si un número es primo y una función primesUpTo que genere una lista de todos los números primos hasta un número dado utilizando rangos y funciones básicas de listas.
+isPrime :: Int -> Bool
+primesUpTo :: Int -> [Int]
+Ejercicio 4: Operaciones sobre listas infinitas
+Escribe una función firstNPrimes que tome un entero n y devuelva una lista con los primeros n números primos utilizando listas infinitas.
+firstNPrimes :: Int -> [Int]
+Ejercicio 5: Combinación y permutación de listas
+Escribe una función combinations que genere todas las combinaciones posibles de un tamaño dado de una lista y una función permutations que genere todas las permutaciones posibles de una lista.
+combinations :: Int -> [a] -> [[a]]
+permutations :: [a] -> [[a]]
+Ejercicio 6: Implementación de ciclos
+Escribe una función cycleN que tome un número n y una lista, y devuelva una nueva lista con la lista original repetida n veces.
+cycleN :: Int -> [a] -> [a]
+--}
