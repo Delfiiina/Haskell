c3f672cfc3ee7e29997dfe49430e7d03e5f41a77
Delfiiina <165937031+Delfiiina@users.noreply.github.com>
2024-09-11 13:53:26 -0300

    Add files via upload

--- segCuatriClases/guia4.hs
+++ segCuatriClases/guia4.hs
@@ -1,39 +1,13 @@
--- EJS CORREGIDOS = 2),7) y el 9) 
--- Ejercicio 2
-parteEntera :: Float -> Integer
-parteEntera n
-    | 0 <= n && n < 1 = 0
-    | otherwise = 1 + parteEntera (n-1)
---Ejercicio 7 
---iesimoDigito :: Integer -> Integer -> Integer
---iesimoDigito n i = ((n `div` (10 ^ ((cantDigitos n) - i))) `mod` 10) SIN RECURSIÓN
-iesimoDigito :: Integer -> Integer -> Integer
-iesimoDigito n 1 = n `div` (10 ^ (cantDigitos(n) - 1))
-iesimoDigito n i = iesimoDigito (n `mod` (10 ^ (cantDigitos(n)-1))) (i-1)
-cantDigitos :: Integer -> Integer
-cantDigitos 0 = 1
-cantDigitos n 
-    | 0 < n && n <= 9 = 1
-    | otherwise = 1 + cantDigitos (n `div` 10)
---Ejercicio 9
-esCapicua :: Integer -> Bool
-esCapicua x
-    | 0 <= x && x < 9 = True
-    | otherwise = not ( (mod x 10 == (x `mod` (10 ^ ((cantDigitos x) - 1))))  && (esCapicua ((x `mod` 10)  `mod` (10 ^ ((cantDigitos x) - 1)))))
-    -- Esta no funciona correctamente
-esCapicuaBien :: Integer -> Bool
-esCapicuaBien x
-    | x < 10 = True
-    | x < 100 = iesimoDigito x 1 == iesimoDigito x 2
-    | otherwise = iesimoDigito x 1 == ultimoDigito && esCapicuaBien sacarPrimeroYUltimo
-    where ultimoDigito = mod x 10
-          sacarPrimeroYUltimo = div (mod x (10 ^ ((cantDigitos x)-1))) 10
---Ejercicio 16 
---a)
+-- Ejercicio 16
+-- a)
 menorDivisor :: Integer -> Integer
 menorDivisor 1 = 1
-menorDivisor n = loDivide   
---b) 
+menorDivisor n = loDivide n 2 
+loDivide :: Integer -> Integer -> Integer
+loDivide n x 
+    | mod n x == 0 = x
+    | otherwise = loDivide n (x+1)
+-- b)
 esPrimo :: Integer -> Bool
 esPrimo 1 = False
 esPrimo n = verSiesPrimo n (n-1)
@@ -45,3 +19,44 @@ esDivisible x y
     | (x - y) == 0 = True
     | (x - y) < 0 = False
     | (x - y) /= 0 = esDivisible (x-y) y 
+esPrimo2 :: Integer -> Bool
+esPrimo2 1 = False
+esPrimo2 n = menorDivisor n == n
+-- c) 
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos a b 
+    | a == b = False
+    | a == 1 || b == 1 = True
+    | otherwise = verSiSonCoprimos a b 2
+verSiSonCoprimos :: Integer -> Integer -> Integer -> Bool
+verSiSonCoprimos a b n
+    | mod a n == 0 && mod b n == 0 = False
+    | otherwise = verSiSonCoprimos a b (n+1)
+-- Ejercicio 14
+{--
+sumaPotencias :: Integer ->Integer ->Integer ->Integer
+sumaPotencias q 1 1 = q ^ 2
+sumaPotencias q n m = (sumaPotenciasFijoM q n m) + (sumaPotenciasFijoN q n m)
+
+sumaPotenciasFijoM :: Integer -> Integer -> Integer -> Integer
+sumaPotenciasFijoM q 1 m = q ^ (1+m)
+sumaPotenciasFijoM q n m = (q ^ (n+m)) + (sumaPotenciasFijoM q (n-1) m) 
+
+sumaPotenciasFijoN :: Integer -> Integer -> Integer -> Integer
+sumaPotenciasFijoN q n 1 = q ^ (n +1) 
+sumaPotenciasFijoN q n m = (q ^ (n+m)) + (sumaPotenciasFijoN q n (m-1))
+--}
+-- No era necesario tener 2 funciones auxiliares.
+sumaPotencias :: Integer ->Integer ->Integer ->Integer
+sumaPotencias q 1 1 = q ^ 2
+sumaPotencias q 1 m = sumaPotenciasFijoM q 1 m 
+sumaPotencias q n m = (sumaPotenciasFijoM q n m) + sumaPotencias q (n-1) m
+
+sumaPotenciasFijoM :: Integer -> Integer -> Integer -> Integer
+sumaPotenciasFijoM q n 0 = 0
+sumaPotenciasFijoM q n 1 = q ^ (1+n)
+sumaPotenciasFijoM q n m = (q ^ (n+m)) + (sumaPotenciasFijoM q n (m-1)) 
+
+-- Ejercicio 19
+esSumaInicialDePrimos :: Integer -> Bool
+esSumaInicialDePrimos 
